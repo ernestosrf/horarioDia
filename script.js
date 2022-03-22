5fb42bd770ae424daf0d23c81870371295cee199
ernestosrf <79682382+ernestosrf@users.noreply.github.com>
2022-03-21 21:00:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function carregar(){
     var hora = data.getHours()
     msg.innerHTML= `Agora são ${hora} horas.`
     if(hora >= 0 && hora <12){
-       img.src = 'manha.png'
+       img.src = 'public/img/manha.png'
        document.body.style.background = '#e2cd9f'
        
     } else if(hora >=12 && hora<18){
-        img.src = 'tarde.png'
+        img.src = 'public/img/tarde.png'
         document.body.style.background = '#b9846f'
     } else{
-        img.src = 'noite.png'
+        img.src = 'public/img/noite.png'
         document.body.style.background = '#515154'
     }
 }
